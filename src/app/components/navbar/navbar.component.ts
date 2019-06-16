52fd641d43ec8ef852159a12af43bb382f3f18c4
marivit <virgivitali@gmail.com>
2019-06-16 12:35:11 -0400

    get user

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
+import { Observable } from 'rxjs';
+import { User } from 'firebase';
 
 @Component({
   selector: 'app-navbar',
@@ -7,14 +9,29 @@ import { AuthService } from 'src/app/services/auth.service';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
-
+  user: firebase.User;
+  user$: Observable<firebase.User>;
+  
   constructor(private authServ: AuthService) { }
 
   ngOnInit() {
+    /*this.authServ.user.subscribe( (user) => {
+      this.user = user;
+      console.log(user);
+      if(user) {
+        console.log(user.displayName);
+      }
+    });*/
+    this.user$ = this.authServ.user;
+
   }
 
-  public GoogleAuth() {
+  public login() {
     this.authServ.loginWithGoogle();
+  }
+
+  logout() {
+    this.authServ.logout();
   }
 
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,15 +1,35 @@
-import { Injectable } from '@angular/core';
+import { Injectable, NgZone } from '@angular/core';
 
 import { AngularFireAuth } from '@angular/fire/auth';
 import * as firebase from 'firebase/app';
 import { auth } from 'firebase/app';
+import { Observable, ReplaySubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(public afAuth: AngularFireAuth) { }
+  private isLoggedIn$ = new ReplaySubject<boolean>(1);
+  private user$ = new ReplaySubject<firebase.User>(1);
+  
+  constructor(public afAuth: AngularFireAuth, private ngZone: NgZone) {
+    this.isLoggedIn$.next(false);
+    this.afAuth.auth.onAuthStateChanged(async (user) => {
+      this.ngZone.run(() => {
+        this.isLoggedIn$.next(Boolean(user));
+        this.user$.next(user);
+      })
+    });
+   }
+
+  get isLoggedIn(): Observable<boolean> {
+    return this.isLoggedIn$.asObservable();
+  }
+
+  get user(): Observable<firebase.User> {
+    return this.user$.asObservable();
+  }
 
   /**
    * Log users in with Google Auth Provider Pop-up
@@ -18,7 +38,10 @@ export class AuthService {
   public loginWithGoogle() {
     this.afAuth.auth.signInWithPopup(new auth.GoogleAuthProvider());
   }
+
   logout() {
     this.afAuth.auth.signOut();
   }
+
+  
 }
